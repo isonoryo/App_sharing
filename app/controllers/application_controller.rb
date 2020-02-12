a9231f93e12992c247e6b9f4e7fc769ec1b862e9
isonoryo <ljiye2422@gmail.com>
2020-02-12 17:57:00 +0900

    ADD ログイン機能実装とプロフィール編集機能実装（途中）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  #ログイン後にプロフィール画面へ
+  def after_sign_in_path_for(resource)
+    "/user/#{current_user.id}"
+  end
+
+  protected    
+
+  def configure_permitted_parameters
+    added_attrs = [ :name, :email, :introduce, :password, :password_confirmation　]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -51,9 +55,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    "/user/#{current_user.id}"
+  end
+
+   #編集後にプロフィール画面へ
+   def after_update_path_for(resource)
+    "/user/#{current_user.id}"
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
