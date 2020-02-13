df522c253bd0cf7e1c0d8576de175a45f03853e9
isonoryo <ljiye2422@gmail.com>
2020-02-13 14:27:27 +0900

    ADD ポートフォリオ投稿機能雛形作成

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,23 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolios = Portfolio.all
+    
+  end
+
+  def new
+    @portfolios = Portfolio.new
+  end
+
+  def create
+    @portfolios = current_user.portfolios.build(portfolio_params)
+    @portfolios.save
+    redirect_to new_portfolio_path
+  end
+
+  private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:name, :comment, :creation_period,
+                                        :study_period, :deploy_url, :github_url)
+    end
 end
