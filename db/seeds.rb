186541ec6b7c6a5842cfa1930d527a16f3dd9638
isonoryo <ljiye2422@gmail.com>
2020-02-21 20:44:13 +0900

    FIX seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# 5.times do |i|
-#   ProgrammingLanguage.create!(name: "sample#{i + 1}")
-#   end
+5.times do |i|
+  ProgrammingLanguage.create!(name: "sample#{i + 1}")
+  end
 
-# 5.times do |i|
-#   School.create!(name: "school#{i + 1}")
-#   end
+5.times do |i|
+  School.create!(name: "school#{i + 1}")
+  end
