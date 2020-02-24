ac9bf7ed7ee69031a029e97574efbbb946e897fb
isonoryo <ljiye2422@gmail.com>
2020-02-24 19:51:45 +0900

    ADD portfolio model_spec追加

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe Portfolio, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Portfolio, type: :model do
+  user = FactoryBot.create(:user)
+  it 'nameが空なら登録できない' do
+    portfolio = user.portfolios.build(user_id: 1, name: '', comment: 'コメント',
+    creation_period:'〜２０時間', study_period: '〜１ヶ月', school_id: 1,
+    deploy_url: 'https://www.yahoo.co.jp/' )
+    expect(portfolio).not_to be_valid
+  end
+
+  it 'commentが空なら登録できない' do
+    portfolio = user.portfolios.build(user_id: 1, name: '1号', comment: '',
+    creation_period:'〜２０時間', study_period: '〜１ヶ月', school_id: 1,
+    deploy_url: 'https://www.yahoo.co.jp/' )
+    expect(portfolio).not_to be_valid
+  end
+
+  it 'creation_periodが空なら登録できない' do
+    portfolio = user.portfolios.build(user_id: 1, name: '1号', comment: 'コメント',
+    creation_period:'', study_period: '〜１ヶ月', school_id: 1,
+    deploy_url: 'https://www.yahoo.co.jp/' )
+    expect(portfolio).not_to be_valid
+  end
+
+  it 'study_periodが空なら登録できない' do
+    portfolio = user.portfolios.build(user_id: 1, name: '1号', comment: 'コメント',
+    creation_period:'〜２０時間', study_period: '', school_id: 1,
+    deploy_url: 'https://www.yahoo.co.jp/' )
+    expect(portfolio).not_to be_valid
+  end
+
+  it 'deployurlが空なら登録できない' do
+    portfolio = user.portfolios.build(user_id: 1, name: '1号', comment: 'コメント',
+    creation_period:'〜２０時間', study_period: '〜１ヶ月', school_id: 1,
+    deploy_url: '' )
+    expect(portfolio).not_to be_valid
+  end
+
+  it 'deployurlが不正な入力なら登録できない' do
+    portfolio = user.portfolios.build(user_id: 1, name: '1号', comment: 'コメント',
+    creation_period:'〜２０時間', study_period: '〜１ヶ月', school_id: 1,
+    deploy_url: 'アイウエオ' )
+    expect(portfolio).not_to be_valid
+  end
+
+  it '必須項目が全て済みであれば、登録できる' do
+    portfolio = user.portfolios.build(user_id: 1, name: '1号', comment: 'コメント',
+    creation_period:'〜２０時間', study_period: '〜１ヶ月', school_id: 1,
+    deploy_url: 'https://www.yahoo.co.jp/' )
+    expect(portfolio).to be_valid
+  end
 end
